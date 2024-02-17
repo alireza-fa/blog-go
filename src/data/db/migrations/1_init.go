e59996826b84be930b04ad3b5c4d45b8c63b60ea
alireza-fa <alirezafeyze44@gmail.com>
2024-02-18 01:15:17 +0330

    add user info init information

--- src/data/db/migrations/1_init.go
+++ src/data/db/migrations/1_init.go
@@ -2,8 +2,10 @@ package migrations
 
 import (
 	"fmt"
+	"github.com/alireza-fa/blog-go/src/constants"
 	"github.com/alireza-fa/blog-go/src/data/db"
 	"github.com/alireza-fa/blog-go/src/data/models"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -21,6 +23,9 @@ func createTables(database *gorm.DB) {
 	tables = addNewTable(database, models.Role{}, tables)
 	tables = addNewTable(database, models.UserRole{}, tables)
 
+	// default data
+	createDefaultUserInfo(database)
+
 	for i := 0; i < len(tables); i++ {
 		err := database.Migrator().CreateTable(tables[i])
 		if err != nil {
@@ -35,4 +40,55 @@ func addNewTable(database *gorm.DB, model interface{}, tables []interface{}) []i
 		tables = append(tables, model)
 	}
 	return tables
+}
+
+func createDefaultUserInfo(database *gorm.DB) {
+	adminRole := models.Role{Name: constants.AdminRole}
+	createRoleIfNotExist(database, &adminRole)
+
+	defaultRole := models.Role{Name: constants.DefaultRole}
+	createRoleIfNotExist(database, &defaultRole)
+
+	var user models.User
+	user.UserName = "blog-go"
+	user.Email = "blog-go@hotmain.com"
+	user.FullName = "blog go"
+
+	passwordByte, err := bcrypt.GenerateFromPassword([]byte("BlogGo11228"), bcrypt.DefaultCost)
+	if err != nil {
+		panic("error while creating default user info" + err.Error())
+	}
+	user.Password = string(passwordByte)
+
+	createUserIfNotExist(database, &user, []int{adminRole.Id, defaultRole.Id})
+}
+
+func createRoleIfNotExist(database *gorm.DB, role *models.Role) {
+	exists := 0
+	database.
+		Model(models.Role{}).
+		Select("1").
+		Where("name = ?", role.Name).
+		First(&exists)
+	if exists == 0 {
+		database.Create(role)
+	}
+}
+
+func createUserIfNotExist(database *gorm.DB, user *models.User, roleIds []int) {
+	exists := 0
+	database.
+		Model(models.User{}).
+		Select("1").
+		Where("user_name = ?", user.UserName).
+		First(&exists)
+	if exists == 0 {
+		database := database.Begin()
+		database.Create(user)
+		for i := 0; i < len(roleIds); i++ {
+			userRole := models.UserRole{UserId: user.Id, RoleId: roleIds[i]}
+			database.Create(&userRole)
+		}
+		database.Commit()
+	}
 }
